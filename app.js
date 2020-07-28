040a8e1bed8257bbd995aeab51a224a5b6f7a0f0
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-28 16:40:20 -0400

    Define another flexible route

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ app.get('/', (req, res) => {
   res.send("Hello from Express!");
 })
 
+app.get("*xyz", (req, res) => {
+  res.send("That's all I wrote.");
+});
+
 app.all('*', (req, res) => {
 
-res.render("index", { method: req.method, path: req.path, randomNumber: Math.floor(Math.random() * 10)})
+  res.render("index", {
+    method: req.method,
+    path: req.path,
+    randomNumber: Math.floor(Math.random() * 10)
+  })
 });
 
 app.listen(8081, () => console.log('Listening on port 8081...'));
