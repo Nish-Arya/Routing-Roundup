b5a2170cbf6f7148eefb85538e479289212a1943
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-28 17:11:25 -0400

    Capture a value from the URL path

--- app.js
+++ app.js
@@ -12,13 +12,17 @@ app.get("*xyz", (req, res) => {
   res.send("That's all I wrote.");
 });
 
-app.all('*', (req, res) => {
+app.get('/capital-letters/:input(\\w+)', (req, res) => {
+  res.send(req.params.input.toUpperCase());
+})
 
+app.all('*', (req, res) => {
   res.render("index", {
     method: req.method,
     path: req.path,
     randomNumber: Math.floor(Math.random() * 10)
   })
 });
+
 
 app.listen(8081, () => console.log('Listening on port 8081...'));
