efb5b7eb0ef2f94dcb0957236f9eba7b07b5754e
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-28 16:08:02 -0400

    Define the default route

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Hello from Express!");
+})
+
+app.listen(8081, () => console.log('Listening on port 8081...'));
