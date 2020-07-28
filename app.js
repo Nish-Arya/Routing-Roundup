0054ea4e6a436bff119f8b7fec2bdcaa7768dd90
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-28 16:31:08 -0400

    Use a template to render HTML

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
   res.send("Hello from Express!");
 })
+
+app.all('*', (req, res) => {
+
+res.render("index", { method: req.method, path: req.path, randomNumber: Math.floor(Math.random() * 10)})
+});
 
 app.listen(8081, () => console.log('Listening on port 8081...'));
