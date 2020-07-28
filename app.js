6b2706f55b99401c52a5e2bff202758fdb882d80
Nishchay Arya <nishchay.arya92@gmail.com>
2020-07-28 17:28:37 -0400

    Define a collection of route handlers

--- app.js
+++ app.js
@@ -16,13 +16,20 @@ app.get('/capital-letters/:input(\\w+)', (req, res) => {
   res.send(req.params.input.toUpperCase());
 })
 
-app.all('*', (req, res) => {
+app.get("*/contact", (req, res) => {
+  res.send("Contact");
+});
+
+app.get("*/bio", (req, res) => {
+  res.send("Bio");
+});
+
+app.all(/^\/[A-Za-z0-9\-_]*$/, (req, res) => {
   res.render("index", {
     method: req.method,
     path: req.path,
-    randomNumber: Math.floor(Math.random() * 10)
-  })
+    randomNumber: Math.floor(Math.random() * 10),
+  });
 });
-
 
 app.listen(8081, () => console.log('Listening on port 8081...'));
